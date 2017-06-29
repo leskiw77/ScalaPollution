96e733c5f345041d11b1f8b9188e8aa83d2fd550
Kamil Borowiec <kborowiec95@gmail.com>
2017-06-30 01:35:34 +0200

    Small corrects

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,6 +5,7 @@ import service.station.GetAllService
 
 object Application extends Controller {
 
+  // Main Action
   def index = Action {
     Ok(views.html.index("Hello", GetAllService.getAll))
   }
